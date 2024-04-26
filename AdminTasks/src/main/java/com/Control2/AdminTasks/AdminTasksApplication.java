4ce8ed33e68596fa3bfe59453c9d6663aee65111
z1ck <jose.foitzick@usach.cl>
2024-04-25 20:04:49 -0400

    Agregando inicio de backend

---
+++ AdminTasks/src/main/java/com/Control2/AdminTasks/AdminTasksApplication.java
@@ -0,0 +1,13 @@
+package com.Control2.AdminTasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminTasksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminTasksApplication.class, args);
+	}
+
+}
---
+++ AdminTasks/src/test/java/com/Control2/AdminTasks/AdminTasksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Control2.AdminTasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminTasksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
