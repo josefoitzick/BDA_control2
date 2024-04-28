477ebe1fc8cd50f83c49b6eb7d6dd790f581f72a
z1ck <jose.foitzick@usach.cl>
2024-04-28 12:02:26 -0400

    Agregando carpeta frontend

---
+++ frontend/pages/login.vue
@@ -0,0 +1,38 @@
+<template>
+    <form @submit.prevent="submit">
+      <h1 class="h3 mb-3 fw-normal">Inicia sesión</h1>
+  
+      <input v-model="username" type="text" class="form-control" placeholder="Nombre de usuario" required>
+  
+      <input v-model="password" type="password" class="form-control" placeholder="Contraseña" required>
+  
+      <button class="w-100 btn btn-lg btn-primary" type="submit">Entrar</button>
+    </form>
+  </template>
+  
+  <script>
+  export default {
+    name: "login",
+    data() {
+      return {
+        username: '',
+        password: ''
+      }
+    },
+    methods: {
+      async submit() {
+        await fetch('http://localhost:8000/auth/login', {
+          method: 'POST',
+          headers: {'Content-Type': 'application/json'},
+          credentials:'include',
+          body: JSON.stringify({
+            username: this.username,
+            password: this.password
+          })
+        });
+  
+        await this.$router.push('/');
+      }
+    }
+  }
+  </script>
---
+++ frontend/pages/register.vue
@@ -0,0 +1,47 @@
+<template>
+    <form @submit.prevent="submit">
+      <h1 class="h3 mb-3 fw-normal">Crea tu cuenta!</h1>
+  
+      
+      <input v-model="username" class="form-control" placeholder="Apodo" required>
+      <input v-model="firstname" class="form-control" placeholder="Nombre" required>
+      <input v-model="lastname" class="form-control" placeholder="Apellido" required>
+      <input v-model="password" type="password" class="form-control" placeholder="Contraseña" required>
+
+      
+  
+      <button class="w-100 btn btn-lg btn-primary" type="submit">Crear cuenta</button>
+    </form>
+  </template>
+  
+  <script>
+  export default {
+    name: "register",
+    data() {
+      return {
+        username: '',
+        firstname:'',
+        lastname: '',
+        password: '',
+        country:'Chile'
+      }
+    },
+    methods: {
+      async submit() {
+        await fetch('http://localhost:8080/auth/register', {
+          method: 'POST',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({
+            username:this.username,
+            firstname:this.firstname,
+            lastname:this.lastname,
+            password:this.password,
+            country:this.country
+          })
+        });
+  
+        await this.$router.push('/login');
+      }
+    }
+  }
+  </script>
